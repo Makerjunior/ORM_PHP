d88f5817c0aafc1482fa9b82ee2141660ce8922b
Makerjunior <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>
2025-09-28 20:59:07 -0300

    exemples

--- config/database.php
+++ config/database.php
@@ -4,11 +4,11 @@
  * Usar .ENV em abiente de produção é altamente recomendado.
  * */
 return [
-    'host'     => "ep-old-haze-ad11r3y5-pooler.c-2.us-east-1.aws.neon.tech",
+    'host'     => "",
     'port'     => 5432,
-    'dbname'   => "neondb",
-    'user'     => "neondb_owner",
+    'dbname'   => "",
+    'user'     => "",
     // Mantendo o formato original da senha com o parâmetro 'endpoint'
-    'password' => "endpoint=ep-old-haze-ad11r3y5-pooler;npg_ArpTF6lym8iL",
+    'password' => "",
     'sslmode'  => 'require' 
 ];
